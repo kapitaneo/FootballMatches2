e452d25d49370f251683fcc4380288e085a11f01
Artem Lupyr <alupyr@hygiena.com>
2024-11-27 10:55:16 -0800

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace FootballMatches.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
